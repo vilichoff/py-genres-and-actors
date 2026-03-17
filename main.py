a1703972c80ac36960f9aacac7867bff32e8a55f
Vladislav <ilichoff88@gmail.com>
2026-03-17 10:45:30 +0200

    Solution

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+import init_django_orm
+from django.db.models import QuerySet
 from db.models import Genre, Actor
 
 
-def main() -> object:
+def main() -> QuerySet:
+    init_django_orm.main()
 
-    genres_to_create = ["Western", "Action", "Dramma"]
-    for genre_name in genres_to_create:
+    genres_to_create = [("Western",), ("Action",), ("Dramma",)]
+    for (genre_name,) in genres_to_create:
         Genre.objects.create(name=genre_name)
 
     actors_to_create = [
-        {"first_name": "George", "last_name": "Klooney"},
-        {"first_name": "Kianu", "last_name": "Reaves"},
-        {"first_name": "Scarlett", "last_name": "Keegan"},
-        {"first_name": "Will", "last_name": "Smith"},
-        {"first_name": "Jaden", "last_name": "Smith"},
-        {"first_name": "Scarlett", "last_name": "Johansson"},
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
     ]
-    for actor_data in actors_to_create:
-        Actor.objects.create(**actor_data)
 
+    for first_name, last_name in actors_to_create:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
+    # Update
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(first_name="George", last_name="Klooney").update(
         last_name="Clooney"
